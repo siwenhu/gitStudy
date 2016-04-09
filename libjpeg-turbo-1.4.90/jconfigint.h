9fbf6c179bb2401832dbb167a13b168ff0df6698
hsw <siwenhu@163.com>
2016-04-08 22:15:32 -0700

    init libjpeg-turbo

---
+++ libjpeg-turbo-1.4.90/jconfigint.h
@@ -0,0 +1,16 @@
+/* jconfigint.h.  Generated from jconfigint.h.in by configure.  */
+/* libjpeg-turbo build number */
+#define BUILD "20160320"
+
+/* How to obtain function inlining. */
+#define INLINE inline __attribute__((always_inline))
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libjpeg-turbo"
+
+/* Version number of package */
+#define VERSION "1.4.90"
+
+/* The size of `size_t', as computed by sizeof. */
+//#define SIZEOF_SIZE_T 8 
+#define SIZEOF_SIZE_T 64
